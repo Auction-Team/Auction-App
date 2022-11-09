68f6335eba06b9a91f7bb4bc490b14fa2d4d4d71
Long Quoc Nguyen <longquoc47krb@gmail.com>
2022-11-09 21:50:41 +0700

    Config and create Account, Product, Order pages.

---
+++ Ecommerce-FE/auction-app-admin/src/helper/routes.js
@@ -0,0 +1,55 @@
+import Loadable from "react-loadable";
+import Loading from "../components/Loading";
+
+const ProductsPage = Loadable({
+  loader: () => import("../pages/Products"),
+  loading: Loading,
+});
+const AddEditProductPage = Loadable({
+  loader: () => import("../pages/AddEditProduct"),
+  loading: Loading,
+});
+const OverviewPage = Loadable({
+  loader: () => import("../pages/Overview"),
+  loading: Loading,
+});
+const OrderPage = Loadable({
+  loader: () => import("../pages/Orders"),
+  loading: Loading,
+});
+const AccountPage = Loadable({
+  loader: () => import("../pages/Account"),
+  loading: Loading,
+});
+const routes = [
+  {
+    element: <AddEditProductPage />,
+    path: "/product/create",
+  },
+  {
+    element: <AddEditProductPage />,
+    path: "/event/update/:eventId",
+  },
+  {
+    element: <AccountPage />,
+    path: "/accounts",
+  },
+  {
+    element: <ProductsPage />,
+    path: "/products",
+  },
+  {
+    element: <OverviewPage />,
+    path: "/",
+  },
+  {
+    element: <OverviewPage />,
+    path: "/overview",
+  },
+  {
+    element: <OrderPage />,
+    path: "/orders",
+  },
+];
+export { ProductsPage, OverviewPage, OrderPage, AddEditProductPage };
+export default routes;
--- Ecommerce-FE/auction-app-admin/src/index.js
+++ Ecommerce-FE/auction-app-admin/src/index.js
@@ -4,15 +4,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/global.scss";
+import "../node_modules/sweetalert2/src/sweetalert2.scss";
 import "./locales/i18n";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
-
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <Provider store={store}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
