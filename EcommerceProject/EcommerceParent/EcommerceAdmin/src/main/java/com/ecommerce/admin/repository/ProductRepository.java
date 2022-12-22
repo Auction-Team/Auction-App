0289099535f958a141ba9a1fdc643eaaaeb04aa5
Phong Hồ <phonghh@unit.com.vn>
2022-12-22 22:16:42 +0700

    [PhongHH] move project

--- EcommerceProject/EcommerceParent/EcommerceAdmin/src/main/java/com/ecommerce/admin/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.admin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.common.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
