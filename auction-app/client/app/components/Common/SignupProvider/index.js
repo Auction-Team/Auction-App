af9ab3acc24c8fa407768b393f1f2073d95adfab
vunh <59404796+vu2872001@users.noreply.github.com>
2022-12-23 21:08:31 +0700

    remove logn with google

--- auction-app/client/app/components/Common/SignupProvider/index.js
+++ auction-app/client/app/components/Common/SignupProvider/index.js
@@ -12,10 +12,10 @@ import { BASE_API_URL } from '../../../constants';
 const SignupProvider = () => {
   return (
     <div className='signup-provider'>
-      <a href={`${BASE_API_URL}/auth/google`} className='mb-2 google-btn'>
+      {/* <a href={`${BASE_API_URL}/auth/google`} className='mb-2 google-btn'>
         <GoogleIcon />
         <span className='btn-text'>Login with Google</span>
-      </a>
+      </a> */}
 
       {/* <a href={`${BASE_API_URL}/auth/facebook`} className='facebook-btn'>
         <FacebookIcon />
